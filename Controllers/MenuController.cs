b4f0313517f2411a8347a4879c1c94e9d0ed55b3
Roman Dolbik-Vorobey <dolbik-w.by@mail.ru>
2020-05-31 22:12:17 +0300

    добавлен контролер menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBC.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Main()
+        {
+            return "<span>Главное меню</span>";
+        }
+
+        public string UserInfo()
+        {
+            return "<span>Меню пользователя</span>";
+        }
+
+        public string Side()
+        {
+            return "<span>боковая панель</span>";
+        }
+
+        public string Map()
+        {
+            return "<span>Карта сайта</span>";
+        }
+    }
+}
