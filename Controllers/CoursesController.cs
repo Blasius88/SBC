9ef2d913eeaddedba6aeefb63b739ce60768d6b0
Roman Dolbik-Vorobey <dolbik-w.by@mail.ru>
2020-06-02 22:03:24 +0300

    4.4

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SBC.DAL;
 
 namespace SBC.Controllers
 {
@@ -11,7 +12,24 @@ namespace SBC.Controllers
         // GET: Courses
         public ActionResult Index()
         {
-            return View();
+            return View(courses);
         }
+
+        List<Courses> courses = new List<Courses>
+        {
+            new Courses
+            {
+                id =1,
+                NameCourses ="Биология",
+                Description = ""
+            },
+            new Courses
+            {
+                id = 2,
+                NameCourses = "Химия",
+                Description = ""
+            }
+
+        };
     }
 }
