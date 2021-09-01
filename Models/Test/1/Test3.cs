85883253c4519fb7d609ba4ddc7b2c853e29a02c
Blasius88 <3803981Blasius>
2021-09-02 02:14:32 +0300

    added thema 13 add thema 13

---
+++ Models/Test/1/Test3.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBC.DAL
+{
+    public class Test3
+    {
+        
+        public string question1 { get; set; }
+        public bool check1 { get; set; }
+        public string question2 { get; set; }
+        public bool check2 { get; set; }
+        public string question3 { get; set; }
+        public bool check3 { get; set; }
+        public string question4 { get; set; }
+        public bool check4 { get; set; }
+        public string question5 { get; set; }
+        public bool check5 { get; set; }
+        public string question6 { get; set; }
+        public bool check6 { get; set; }
+        public string question7 { get; set; }
+        public bool check7 { get; set; }
+        public string question8 { get; set; }
+        public bool check8 { get; set; }
+        public string question9 { get; set; }
+        public bool check9 { get; set; }
+        public string question10 { get; set; }
+        public bool check10 { get; set; }
+        public string question11 { get; set; }
+        public bool check11 { get; set; }
+        public string question12 { get; set; }
+        public bool check12 { get; set; }
+        public string question13 { get; set; }
+        public bool check13 { get; set; }
+        public string question14 { get; set; }
+        public bool check14 { get; set; }
+        public string question15 { get; set; }
+        public bool check15 { get; set; }
+        public string question16 { get; set; }
+        public bool check16 { get; set; }
+        public string question17 { get; set; }
+        public bool check17 { get; set; }
+        public string question18 { get; set; }
+        public bool check18 { get; set; }
+        public string question19 { get; set; }
+        public bool check19 { get; set; }
+        public string question20 { get; set; }
+        public bool check20 { get; set; }
+        public string question21 { get; set; }
+        public bool check21 { get; set; }
+        public string question22 { get; set; }
+        public bool check22 { get; set; }
+        public string question23 { get; set; }
+        public bool check23 { get; set; }
+        public string question24 { get; set; }
+        public bool check24 { get; set; }
+        public string question25 { get; set; }
+        public bool check25 { get; set; }
+        public string question26 { get; set; }
+        public bool check26 { get; set; }
+        public string question27 { get; set; }
+        public bool check27 { get; set; }
+        public string question28 { get; set; }
+        public bool check28 { get; set; }
+        public string question29 { get; set; }
+        public bool check29 { get; set; }
+        public string question30 { get; set; }
+        public bool check30 { get; set; }
+        public string question31 { get; set; }
+        public bool check31 { get; set; }
+        public string question32 { get; set; }
+        public bool check32 { get; set; }
+        public string question33 { get; set; }
+        public bool check33 { get; set; }
+        public string question34 { get; set; }
+        public bool check34 { get; set; }
+        public string question35 { get; set; }
+        public bool check35 { get; set; }
+        public string question36 { get; set; }
+        public bool check36 { get; set; }
+        public string question37 { get; set; }
+        public bool check37 { get; set; }
+        public string question38 { get; set; }
+        public bool check38 { get; set; }
+        public string question39 { get; set; }
+        public bool check39 { get; set; }
+        public string question40 { get; set; }
+        public bool check40 { get; set; }
+        public string question41 { get; set; }
+        public bool check41 { get; set; }
+        public string question42 { get; set; }
+        public bool check42 { get; set; }
+        public string question43 { get; set; }
+        public bool check43 { get; set; }
+        public string question44 { get; set; }
+        public bool check44 { get; set; }
+        public string question45 { get; set; }
+        public bool check45 { get; set; }
+        public string question46 { get; set; }
+        public bool check46 { get; set; }
+        public string question47 { get; set; }
+        public bool check47 { get; set; }
+        public string question48 { get; set; }
+        public bool check48 { get; set; }
+        public string question49 { get; set; }
+        public bool check49 { get; set; }
+        public string question50 { get; set; }
+        public bool check50 { get; set; }
+
+    }
+}
